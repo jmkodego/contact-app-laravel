edb2dff247536396ac9466c0a4084e7ad4e83ab7
jmkodego <jm.baldonado@kodego.ph>
2023-03-11 14:51:46 +0800

    add resource controller for company

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +23,13 @@ Route::get('/', WelcomeController::class);
 
 Route::controller(ContactController::class)->name('contacts.')->group(function () {
     Route::get('/contacts', 'index')->name('index');
-    Route::get('/contacts/{id}', 'show')->name('show');
     Route::get('/contacts/create', 'create')->name('create');
+    Route::get('/contacts/{id}', 'show')->name('show');
 });
+
+Route::resource('/companies', CompanyController::class);
+
+Route::resources([
+    '/tags' => TagController::class,
+    '/tasks' => TaskController::class
+]);
