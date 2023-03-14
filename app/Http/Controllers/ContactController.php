69da40cc28e43babd9c50a9013e6c422723797ee
jmkodego <jm.baldonado@kodego.ph>
2023-03-14 20:34:25 +0800

    refactor static data to connect with backend data

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use App\Repositories\CompanyRepository;
 use Illuminate\Http\Request;
 
@@ -13,20 +14,11 @@ class ContactController extends Controller
     {
     }
 
-    protected function getContacts()
-    {
-        return [
-            1 => ['id' => 1, 'name' => 'John Doe', 'phone' => '1234567890'],
-            2 => ['id' => 2, 'name' => 'Jane Doe', 'phone' => '0987654321'],
-            3 => ['id' => 3, 'name' => 'John Smith', 'phone' => '5551234556'],
-        ];
-    }
-
     public function index(CompanyRepository $company)
     {
 
         $companies = $this->company->pluck();
-        $contacts = $this->getContacts();
+        $contacts = Contact::latest()->get();
         return view('contacts.index', compact('contacts', 'companies'));
     }
 
@@ -37,9 +29,7 @@ class ContactController extends Controller
 
     public function show($id)
     {
-        $contacts = $this->getContacts();
-        abort_if(!isset($contacts[$id]), 404);
-        $contact = $contacts[$id];
+        $contact = Contact::findOrFail($id);
         return view('contacts.show')->with('contact', $contact);
     }
 }
